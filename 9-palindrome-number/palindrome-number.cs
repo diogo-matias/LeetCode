332129c7e992e249ab10c73a422f70b17981497a
Diogo Matias <101677850+diogo-matias@users.noreply.github.com>
2025-04-25 09:10:20 -0300

    Time: 1 ms (99.89%) | Memory: 33.9 MB (99.85%) - LeetSync

--- 9-palindrome-number/palindrome-number.cs
+++ 9-palindrome-number/palindrome-number.cs
@@ -1,8 +1,17 @@
 public class Solution {
     public bool IsPalindrome(int x) {
-        string str = x.ToString();
-        string strInverted = new String(str.Reverse().ToArray());
+        if (x < 0) return false;
 
-        return str == strInverted;
+        int original = x;
+        int reverso = 0;
+
+        while (x > 0)
+        {
+            int digito = x % 10;
+            reverso = reverso * 10 + digito;
+            x /= 10;
+        }
+
+        return original == reverso;
     }
 }
