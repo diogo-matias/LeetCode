e8f6e3566ad7197454246863aa58c6e6356697b6
Diogo Matias <101677850+diogo-matias@users.noreply.github.com>
2025-05-19 08:56:15 -0300

    Time: 4 ms (96.88%) | Memory: 43.6 MB (84.38%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cs
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cs
@@ -3,18 +3,18 @@ public class Solution {
         int count = 0;
 
         for (int i = 0; i < words.Length; i++) {
-            var word1 = words[i];
-            int len1 = word1.Length;
+            var str1 = words[i];
+            int len1 = str1.Length;
 
             for (int j = i + 1; j < words.Length; j++) {
-                var word2 = words[j];
-                int len2 = word2.Length;
+                var str2 = words[j];
+                int len2 = str2.Length;
 
                 if (len1 > len2) continue;
 
                 bool isPrefix = true;
                 for (int k = 0; k < len1; k++) {
-                    if (word1[k] != word2[k]) {
+                    if (str1[k] != str2[k]) {
                         isPrefix = false;
                         break;
                     }
@@ -24,7 +24,7 @@ public class Solution {
 
                 bool isSuffix = true;
                 for (int k = 0; k < len1; k++) {
-                    if (word1[k] != word2[len2 - len1 + k]) {
+                    if (str1[k] != str2[len2 - len1 + k]) {
                         isSuffix = false;
                         break;
                     }
