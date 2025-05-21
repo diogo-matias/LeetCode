c117e801b4df953c6ef94a2b878fb0c030b0dab2
Diogo Matias <101677850+diogo-matias@users.noreply.github.com>
2025-05-21 16:57:04 -0300

    Time: 3 ms (16.41%) | Memory: 42.8 MB (94.53%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cs
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cs
@@ -3,11 +3,27 @@ public class Solution {
         int count = 0;
 
         foreach (var word in words) {
-            if (word.StartsWith(pref)) {
+            if (startsWith(word, pref)) {
                 count++;
             }
         }
 
         return count;
+    }
+
+    public bool startsWith(string word, string pref){
+        if (pref == null) { 
+            return false;
+        }
+
+        if (pref.Length > word.Length) {
+            return false;
+        }
+
+        for(var i = 0; i < pref.Length; i++){
+            if (word[i] != pref[i]) return false;
+        }
+
+        return true;
     }
 }
